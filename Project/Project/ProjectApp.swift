b3f3a477eac2c72650de7f4a370e7d6ca68b7523
Mikhail Zhelnin <mishazhelnin378@gmail.com>
2026-05-02 17:04:56 +1000

    Some simple stuff, will create new branch now

--- Project/Project/ProjectApp.swift
+++ Project/Project/ProjectApp.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct ProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomePageView()
         }
     }
 }
-// This is the applications entry point.
+
