ba1c5f58aaa2f2b3b5d98fbe4ab29f698d77fa5f
Mikhail Zhelnin <mishazhelnin378@gmail.com>
2026-05-02 17:54:48 +1000

    Made simple home page.

--- Project/Project/ProjectApp.swift
+++ Project/Project/ProjectApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct ProjectApp: App {
+    @StateObject private var HViewModel = HomeViewModel()
+    
     var body: some Scene {
         WindowGroup {
             HomePageView()
+                .environmentObject(HViewModel)
         }
     }
 }
