0328f7daebd8a26be4983bcc44465e19b3aae44b
004-bigal <alikorgun@gmail.com>
2026-05-11 21:14:04 +1000

    Changed let to var to remove immutable property issue. Also removed semicolons as they do not serve any functional purpose

--- Project/Project/Models/BookSessionModel.swift
+++ Project/Project/Models/BookSessionModel.swift
@@ -8,42 +8,42 @@
 import Foundation
 // This enum represents the different stages of a booking, from processing to finished
 enum BookStatus: String, Codable {
-    case Booked; // Gotten into confirmation screen
-    case Paid;
-    case Finished;
+    case Booked // Gotten into confirmation screen
+    case Paid
+    case Finished
 }
 // Stores information about a dish with three variables; unique ID, dish ID and quantity
 struct DishDetails: Codable, Identifiable {
-    let id = UUID();
-    let dishID: UUID;
-    var quantity: Int;
+    var id = UUID()
+    let dishID: UUID
+    var quantity: Int
 }
 
 // Contains details of a single booking
 struct BookSessionModel: Codable, Identifiable {
     let id: UUID;
     // Table details
-    var tableNo: Int?;
-    var resvTime: Date;
-    var numPeople: Int;
-    var bookingName: String;
+    var tableNo: Int?
+    var resvTime: Date
+    var numPeople: Int
+    var bookingName: String
     
     // Default constructor
     init() {
         id = UUID();
-        tableNo = nil;
-        resvTime = Date.tomorrow.setTime(hour: 11, minute: 0);
-        numPeople = 1;
-        bookingName = "";
+        tableNo = nil
+        resvTime = Date.tomorrow.setTime(hour: 11, minute: 0)
+        numPeople = 1
+        bookingName = ""
     }
     
     // Assist in pushing into db
     init(table: Int, Time: Date, People: Int, Name: String) {
-        id = UUID();
-        tableNo = table;
-        resvTime = Time;
-        numPeople = People;
-        bookingName = Name;
+        id = UUID()
+        tableNo = table
+        resvTime = Time
+        numPeople = People
+        bookingName = Name
     }
     
     // Ordering of each dish: currently not supported
