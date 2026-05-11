83ccecd5f8b5aa81b4e1116807f6ea29229172fe
Charlie Chou <z5474967@ad.unsw.edu.au>
2026-05-11 14:41:34 +1000

    added extra view for visuals

---
+++ Project/Project/Views/Tables/MyBookingsView.swift
@@ -0,0 +1,25 @@
+//
+//  MyBookingsView.swift
+//  Project
+//
+//  Created by CLChou on 2026/5/11.
+//
+
+import SwiftUI
+
+// Shows current personal bookings
+struct BookingItem: View {
+    var body: some View {
+        
+    }
+}
+
+struct MyBookingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyBookingsView()
+}
