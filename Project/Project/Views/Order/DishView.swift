1905900af3968d9395879ab6ec34415321b0bee8
004-bigal <alikorgun@gmail.com>
2026-05-12 12:43:42 +1000

    Split the OrderView file into two files; OrderView and DishView, enabling cleaner organisation

---
+++ Project/Project/Views/Order/DishView.swift
@@ -0,0 +1,20 @@
+//
+//  DishView.swift
+//  Project
+//
+//  Created by Alik Deniz Orgun on 12/5/2026.
+//
+
+import SwiftUI
+
+struct DishView: View {
+    // List the picture then
+    var body: some View {
+        // Picture, title, rightmost is a increment bar that modifies for each dish
+        EmptyView()
+    }
+}
+
+#Preview {
+    DishView()
+}
--- Project/Project/Views/Order/OrderView.swift
+++ Project/Project/Views/Order/OrderView.swift
@@ -7,14 +7,6 @@
 
 import SwiftUI
 
-struct DishView: View {
-    // List the picture then
-    var body: some View {
-        // Picture, title, rightmost is a increment bar that modifies for each dish
-        EmptyView()
-    }
-}
-
 struct OrderView: View {
     var body: some View {
         // Scroll Box that has for each...
